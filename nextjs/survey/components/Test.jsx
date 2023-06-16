9fb8b0269c27600f04a4dec07fa9b4081c0448ac
thadinh <thadinh@strongtie.com>
2023-06-16 16:17:41 +0700

    make test view

--- nextjs/survey/components/Test.jsx
+++ nextjs/survey/components/Test.jsx
@@ -2,23 +2,20 @@ import styles from '@/styles/Common.module.css'
 import styleTest from '@/styles/Test.module.css'
 import React from "react";
 const Test = (props) => {
-    console.log(props)
     return (
         <div className={styleTest.quizOptions}>
             <div>
-                <h3>Pure CSS Quiz</h3>
                 <p>QUESTION 1 OF 5</p>
                 <p>{props.test.question}</p>
             </div>
-            <form>
                 <div>
                 {
-                    props.test.answer.map(item => 
+                    props.test.answer.map((item, idx) => 
                         {
                             return (
-                                <div className={styles.block, styleTest.text}>
-                                    <input type="radio"  id="one-a" name="yes-1" required/>
-                                    <label class="radio-label jsjwjdwjdwjdwco" for="one-a">
+                                <div key={idx} className={styles.block, styleTest.text}>
+                                    <input type="radio"  id="one-a" name="yes-1"/>
+                                    <label>
                                     {item.text}
                                     </label>
                                 </div>
@@ -27,9 +24,8 @@ const Test = (props) => {
                     )
                 }
                 </div>
-                <a id="btn" type="submit" onclick="window.location.href='#section-2'">Next</a>
-            </form>
-         
+                <button style={{padding: 10}} id="btn" onClick={() => { props.onPrevious()}}>Previous</button>
+                <button style={{padding: 10}} id="btn" onClick={() => { props.onNext()}}>Next</button>
         </div>
     );
 };
--- nextjs/survey/pages/quiz.jsx
+++ nextjs/survey/pages/quiz.jsx
@@ -1,29 +1,87 @@
 import Test from '../components/Test'
+import React, { useState } from 'react';
 const Quiz = () => {
-    const quiz = [
+    const initState = {
+        currentStep: 1,
+        tests : [
         {
-            question : 'how old are you ?',
-            answer: [
+            step : 1,
+            active: true,
+            options :
                 {
-                    text : '12'
-                },
-                {
-                    text : '23'
-                },
-                {
-                    text : '56'
+                    question : 'how old are you ?',
+                    answer: [
+                        {
+                            text : '12'
+                        },
+                        {
+                            text : '23'
+                        },
+                        {
+                            text : '56'
+                        }
+                    ]
                 }
-            ]
+        },
+        {
+            step : 2,
+            active: false,
+            options :
+                {
+                    question : 'what your name ?',
+                    answer: [
+                        {
+                            text : 'Matt'
+                        },
+                        {
+                            text : 'Lisa'
+                        },
+                        {
+                            text : 'Josh'
+                        }
+                    ]
+                }
         }
-    ];
+    ]};
+    const [quiz, setQuiz] = useState(initState);
+    const nextQuiz = () =>
+    {
+        const step = quiz.currentStep + 1
+        if(step > quiz.tests.length+1)
+            return;
+        const newQuiz = {...quiz, ...{currentStep : step}};
+        setQuiz(newQuiz);
+    }
+
+    const previousQuiz = () =>
+    {
+        const step = quiz.currentStep - 1
+        if(step == 0)
+            return;
+        const newQuiz = {...quiz, ...{currentStep : step}};
+        setQuiz(newQuiz);
+    }
+
+    const submitAnswer = () => 
+    {
+        alert('submited');
+    }
+
+    if(quiz.currentStep > quiz.tests.length) {
+        return (<button style={{padding: 10}} onClick={submitAnswer}>Submit</button>)
+    } else {
+        return (<div>{
+            quiz.tests.map(item => {
+                if(quiz.currentStep === item.step)
+                {
+                    return (
+                        <Test test={item.options} onNext={nextQuiz} onPrevious={previousQuiz}/>
+                    );
+                }
+            })
+        }</div>);
         
-    
-    return (<div>{
-        quiz.map(item => {
-            return (
-                <Test test={item}/>
-            );
-        })
-    }</div>);
+    }
+   
   };
   export default Quiz;
