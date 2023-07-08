10f88040b0502b3f65bc593b0871e825ad01e935
dinhvanthanh <dinhvanthanh1995@gmail.com>
2023-07-08 12:06:14 +0700

    .

--- nextjs/survey/services/quiz.js
+++ nextjs/survey/services/quiz.js
@@ -1,45 +1,8 @@
 import useSWR from "swr";
 
-export default function getQuiz() {
-  return {
-    currentStep: 1,
-    tests: [
-      {
-        step: 1,
-        active: true,
-        options: {
-          question: "how old are you ?",
-          answer: [
-            {
-              text: "12",
-            },
-            {
-              text: "23",
-            },
-            {
-              text: "56",
-            },
-          ],
-        },
-      },
-      {
-        step: 2,
-        active: false,
-        options: {
-          question: "what your name ?",
-          answer: [
-            {
-              text: "Matt",
-            },
-            {
-              text: "Lisa",
-            },
-            {
-              text: "Josh",
-            },
-          ],
-        },
-      },
-    ],
-  };
+export default async function getQuiz() {
+  const response = await axios.get("http://localhost:8000/users");
+
+  // Parse and process the response data as needed
+  return response.data;
 }
