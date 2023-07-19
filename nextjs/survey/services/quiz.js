b2d0db166da53f1e2289977c6500ad7b006dbeab
dinhvanthanh <dinhvanthanh1995@gmail.com>
2023-07-19 22:22:20 +0700

    .

--- nextjs/survey/services/quiz.js
+++
@@ -1,8 +0,0 @@
-import useSWR from "swr";
-
-export default async function getQuiz() {
-  const response = await axios.get("http://localhost:8000/users");
-
-  // Parse and process the response data as needed
-  return response.data;
-}
--- nextjs/survey/services/serverservive.js
+++
@@ -1,5 +0,0 @@
-import getQuiz from "./quiz";
-export const getServerSideProps = async () => {
-  const data = getQuiz();
-  return { props: { data } };
-};
