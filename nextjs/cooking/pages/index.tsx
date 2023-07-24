ac6648487a0f1b4687f163b9a2539aea5b69754f
thadinh <thadinh@strongtie.com>
2023-07-24 17:24:13 +0700

    card

--- nextjs/cooking/pages/index.tsx
+++ nextjs/cooking/pages/index.tsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/navbar.tsx";
 import Category from "../components/category.tsx";
 import Image from "next/image";
+import Recipe from "../components/recipe.tsx";
 export default function Home() {
   return (
     <div className="h-screen">
       <Navbar />
       <Category />
+      <div>
+        <Recipe />
+      </div>
     </div>
   );
 }
